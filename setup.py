5843c83315fb996f2af02b78215042bee14e68e3
flamel <5rK0vVjNYRxx176qP15R>
2018-11-09 13:11:10 +0100

    include_package_data

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from setuptools import setup, find_packages
 setup(name='customaccount',
       version='0.1',
       packages=find_packages(),
+      package_data={
+          'customaccount': ['templates/*.html', 'templates/**/*.html']
+      },
       include_package_data=True,
       )
