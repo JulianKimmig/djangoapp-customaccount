f8953184a91b908bea2b237b3c67b98a0c44e6a0
flamel <5rK0vVjNYRxx176qP15R>
2018-11-09 12:34:18 +0100

    mak euser nullable

--- customaccount/models.py
+++ customaccount/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 class CustomUser(models.Model):
-    user = models.ForeignKey(User, verbose_name=_("User"),on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, verbose_name=_("User"),on_delete=models.SET_NULL,null=True)
     deleted = models.BooleanField(default=False)
     inactive = models.BooleanField(default=False)
     public_name= models.CharField(max_length=20,verbose_name=_("Public Name"))
