f4331a16305ce4c7553172bbb81c1219c6bb8a77
flamel <5rK0vVjNYRxx176qP15R>
2018-11-09 12:14:48 +0100

    Custom User Model added

--- customaccount/models.py
+++ customaccount/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+class CustomUser(models.Model):
+    user = models.ForeignKey(User, verbose_name=_("User"),on_delete=models.SET_NULL)
+    deleted = models.BooleanField(default=False)
+    inactive = models.BooleanField(default=False)
+    public_name= models.CharField(max_length=20,verbose_name=_("Public Name"))
